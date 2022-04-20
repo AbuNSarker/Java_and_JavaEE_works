06bdf283550b5aa2def039e151877b33b1985e78
abunsarker <ans363@mun.ca>
2022-04-20 06:02:01 -0230

    HackerRank Java 30 days

---
+++ HackerRank_Java_30Days/Day_04/Day04_ClassVsIntance.java
@@ -0,0 +1,23 @@
+package Day_04;
+
+import java.util.Scanner;
+
+public class Day04_ClassVsIntance {
+	 public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+	        int T = sc.nextInt();
+	        for (int i = 0; i < T; i++) {
+	            int age = sc.nextInt();
+	            Person personObj = new Person(age);
+	            personObj.amIold();
+	            for (int j = 0; j < 3; j++) {
+	            	personObj.yearPasses();
+	            }
+	            personObj.amIold();
+	            System.out.println();
+	        }
+	        sc.close();
+
+	    }
+
+	}
